14f6fb9434f1cdc5a946a904c21e1834bdd9f96b
EricMesquita <eric.oliveira.mesquita@gmail.com>
2018-06-24 14:10:48 -0300

    Correção na exibição da mensagem de erro

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -33,122 +33,132 @@
           <md-button to="../password/redefinirsenha">ESQUECI MINHA SENHA</md-button>
           <md-button to="./cadastro">QUERO ME CADASTRAR </md-button>
         </div>
-
-        <md-snackbar :md-active.sync="errorSaved">{{ error }}</md-snackbar>
-
+        <div v-show="errorSaved">
+         <md-snackbar :md-active.sync="errorSaved">{{ error }}</md-snackbar>
+        </div>
       </md-card>
     </form>
   </div>
 </template>
 
 <script>
-  import { validationMixin } from 'vuelidate'
-  import {
-    required,
-    email,
-    minLength,
-    maxLength
-  } from 'vuelidate/lib/validators'
+import { validationMixin } from "vuelidate";
+import {
+  required,
+  email,
+  minLength,
+  maxLength
+} from "vuelidate/lib/validators";
 
-  export default {
-    name: 'Login',
-    mixins: [validationMixin],
-    data: () => ({
-      form: {
-        email: null,
-        password: null,
-      },
-      userSaved: false,
-      sending: false,
-      lastUser: null,
-      errorSaved: false
-    }),
-    validations: {
-      form: {
-        email: {
-          required,
-          email
-        },
-        password: {
-          required,
-          minLength: minLength(3)
-        }
-      }
+export default {
+  name: "Login",
+  mixins: [validationMixin],
+  data: () => ({
+    form: {
+      email: null,
+      password: null
     },
-    computed: {
-      error () {
-        return this.$store.getters.erro != null ? this.$store.getters.erro.message : null ;
-      }
-    },
-    methods: {
-      getValidationClass (fieldName) {
-        const field = this.$v.form[fieldName]
-
-        if (field) {
-          return {
-            'md-invalid': field.$invalid && field.$dirty
-          }
-        }
+    userSaved: false,
+    sending: false,
+    lastUser: null,
+    errorSaved: false
+  }),
+  validations: {
+    form: {
+      email: {
+        required,
+        email
       },
-      clearForm () {
-        this.$v.$reset()
-        this.form.password = null
-        this.form.email = null
-      },
-      saveUser () {
-        this.sending = true
-        this.$store.dispatch('login', {email: this.form.email, password: this.form.password})
-        .catch(erro => 
-          console.logn(erro),
-          this.setError(),
-          this.sending = false
-        )
-      },
-      validateUser () {
-        this.$v.$touch()
-
-        if (!this.$v.$invalid) {
-          this.saveUser()
-        }
-      },
-      setError(){
-        this.errorSaved = true
+      password: {
+        required,
+        minLength: minLength(3)
       }
     }
+  },
+  computed: {
+    error() {
+      return this.$store.getters.erro != null
+        ? this.$store.getters.erro.message
+        : null;
+    }
+  },
+  methods: {
+    getValidationClass(fieldName) {
+      const field = this.$v.form[fieldName];
+
+      if (field) {
+        return {
+          "md-invalid": field.$invalid && field.$dirty
+        };
+      }
+    },
+    clearForm() {
+      this.$v.$reset();
+      this.form.password = null;
+      this.form.email = null;
+    },
+    saveUser() {
+      this.sending = true;
+      this.$store
+        .dispatch("login", {
+          email: this.form.email,
+          password: this.form.password
+        })
+        .catch(error => {
+          console.log(error)
+          if (error.response.data.status == 500) {
+            error.response.data.message = "Usuário ou Senha incorretas";
+          }
+          this.$store.dispatch("setError", error.response.data);
+          this.setError();
+          this.sending = false;
+        });
+    },
+    validateUser() {
+      this.$v.$touch();
+
+      if (!this.$v.$invalid) {
+        this.saveUser();
+      }
+    },
+    setError() {
+      this.errorSaved = true;
+    }
   }
+};
 </script>
 
 <style lang="scss" scoped>
-  .md-progress-bar {
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-  }
+.md-progress-bar {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+}
 
 //Global
 .md-card-header {
-	background: #2687e9;
-	color: #fff;
+  background: #2687e9;
+  color: #fff;
 }
 
 .md-button.md-dense {
-	width: 100%;
+  width: 100%;
 }
 
 .md-card-content {
-	padding-left: 50px;
-	padding-right: 50px;
+  padding-left: 50px;
+  padding-right: 50px;
   padding-top: 15px;
 }
 
 .md-card-actions {
-	padding-left: 50px;
-	padding-right: 50px;
+  padding-left: 50px;
+  padding-right: 50px;
 }
 
-.md-layout{
-  padding-left: 60px;
+.md-layout {
+  padding-left: 40px;
 }
 //!Global
 </style>
