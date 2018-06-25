d7458c3db302cd935a37dc74682dcbf9cdd488e2
afabianoo <afabiano97@gmail.com>
2018-06-25 20:31:20 -0300

    realizado listagem de gastos

--- src/components/Dashboard/RefundExpenseItem.vue
+++ src/components/Dashboard/RefundExpenseItem.vue
@@ -1,19 +1,17 @@
 <template>
-      <md-table-row slot="md-table-row" :slot-scope="reembolso" md-selectable="multiple" md-auto-select>
-        <md-table-cell md-label="Email" md-sort-by="email">{{ reembolso.email }}</md-table-cell>
-        <md-table-cell md-label="Reembolso" md-sort-by="Reembolso">{{ reembolso.nome }}</md-table-cell>
-        <md-table-cell md-label="Status" md-sort-by="status"><status-reembolso :status="reembolso.status"></status-reembolso></md-table-cell>
-        <md-table-cell md-label="Valor" md-sort-by="valor">{{ reembolso.valor }}</md-table-cell>
-        <md-table-cell md-label="Categoria" md-sort-by="categoria">{{ reembolso.categoria }}</md-table-cell>
-        <md-table-cell md-label="User" md-sort-by="user">{{ reembolso.user }}</md-table-cell> 
-      </md-table-row> 
+
+      <md-table-row slot="md-table-row" :slot-scope="refund" md-selectable="multiple" md-auto-select>
+        <md-table-cell md-label="Email" md-sort-by="email">{{ refund.email }}</md-table-cell>
+        <md-table-cell md-label="Nome" md-sort-by="Nome">{{ refund.userName }}</md-table-cell>
+        <md-table-cell md-label="Valor" md-sort-by="valor">{{ refund.value }}</md-table-cell>
+       </md-table-row> 
 </template>
 
 <script>
   import Status from './RefundStatus.vue';
 
   export default {
-    props: ['reembolso'],
+    props: ['refund'],
     components: {
       'status-reembolso': Status
     }
